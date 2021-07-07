c456fb645c0aab50d416dbf4269488eb5699f958
NagaSatish <dev02@retrieveai.com>
2021-07-07 23:22:56 +0530

    updated packages, app config

--- public/private/appconfig.js
+++ public/private/appconfig.js
@@ -2,6 +2,13 @@ window["appconfig"] = {
     shopify: {
       endpoint: "https://tiwneavcyd.execute-api.us-west-2.amazonaws.com",
       envpath: "/sbdev",
-      resource: "/activate"
+      resource: "/activate",
+      convo: "/conversations",
+      cust: "/customers",
+      analytics: "/analytics",
+      themes: "/themes",
+      contactUrl: "https://sleekbuys.com",
+      privacyUrl: "https://sleekbuys.com",
+      // cookie: "window.location.origin",
     }
 }
