b0f8dc59f44484df37b1cf24cfda90649291533e
NagaSatish <dev02@retrieveai.com>
2021-07-14 16:25:50 +0530

    added billing

--- public/private/appconfig.js
+++ public/private/appconfig.js
@@ -1,14 +1,15 @@
 window["appconfig"] = {
-    shopify: {
-      endpoint: "https://tiwneavcyd.execute-api.us-west-2.amazonaws.com",
-      envpath: "/sbdev",
-      resource: "/activate",
-      convo: "/conversations",
-      cust: "/customers",
-      analytics: "/analytics",
-      themes: "/themes",
-      contactUrl: "https://sleekbuys.com",
-      privacyUrl: "https://sleekbuys.com",
-      // cookie: "window.location.origin",
-    }
+  shopify: {
+    endpoint: "https://ov8166jdvh.execute-api.us-west-2.amazonaws.com",
+    envpath: "/sbshopifydev/sbapi",
+    resource: "/activate",
+    convo: "/conversations",
+    cust: "/customers",
+    analytics: "/analytics",
+    themes: "/themes",
+    billing: "/billing",
+    contactUrl: "https://sleekbuys.com",
+    privacyUrl: "https://sleekbuys.com",
+    // cookie: "window.location.origin",
+  }
 }
