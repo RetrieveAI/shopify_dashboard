de091bb1b45d6e09b18c48b11768d435fc196733
NagaSatish <dev02@retrieveai.com>
2021-07-16 18:16:59 +0530

    added count status and disable button if expired

--- src/Dashboard/Subscriptions.js
+++ src/Dashboard/Subscriptions.js
@@ -1,4 +1,4 @@
-import { Badge, Card, ChoiceList, Layout, Page, TextStyle, Button, FooterHelp, Link } from '@shopify/polaris'
+import { Badge, Card, ChoiceList, Layout, Page, Heading, TextStyle, Banner, Subheading, Button, FooterHelp, Link } from '@shopify/polaris'
 import React, { useCallback, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import useConfig from '../hooks/useConfig';
@@ -17,27 +17,42 @@ const Subscriptions = () => {
 
     // const [billingURL, setBillingURL] = useState('');
 
-    // const [billingStatus, setBillingStatus] = useState(true);
+    const [countStatus, setCountStatus] = useState('');
+
+    const [showBanner, setShowBanner] = useState(false);
+
 
     const [selected, setSelected] = useState([]);
+
+    const planLimits = {
+        Basic: 5000,
+        Pro: 50000,
+        Premium: 200000
+    }
     
 
-    const handleChange = useCallback((value) => setSelected(value), []);
+    const handleChange = useCallback((value) => {
+        return setSelected(value)
+    },  []);
 
-    // useEffect(async () => {
-
-    //   }, [selected]);
+    useEffect(() => {
+        console.log(planLimits[selected[0]], countStatus.current_month_visitors_count );
+        if(countStatus.current_month_visitors_count > planLimits[selected[0]]){
+            setShowBanner(true)
+        }else setShowBanner(false)
+      }, [selected, countStatus]);
+      
 
     //console.log(selected);
 
-    const TrialPlan = {label: 'Trial', value: 'Trial', helpText:
+    const TrialPlan = {label: 'Free Trial', value: 'Trial', helpText:
     'Free Trial for 14 days',};
     const BasicPlan = {label: 'Basic', value: 'Basic',  helpText:
     '$24.99 per month | Up to 5,000 Monthly Site Visitors | Unlimited Sales,Support and Order Tracking Chats | Unlimited AI Chat Interactions',};
     const ProPlan = {label: 'Pro', value: 'Pro',  helpText:
     '$59.99 per month | Up to 50,000 Monthly Site Visitors | Unlimited Sales,Support and Order Tracking Chats | Unlimited AI Chat Interactions',};
     const PremiumPlan = {label: 'Premium', value: 'Premium',  helpText:
-    '$149.99 per month | Up to 2,00,000 Monthly Site Visitors | Unlimited Sales,Support and Order Tracking Chats | Unlimited AI Chat Interactions',};
+    '$149.99 per month | Up to 200,000 Monthly Site Visitors | Unlimited Sales,Support and Order Tracking Chats | Unlimited AI Chat Interactions',};
 
     const handleClick = () => {
         let result;
@@ -48,13 +63,19 @@ const Subscriptions = () => {
             ).then(res => {
                 console.log(res.data);
                 window.open(res.data, "_parent");
-            })   
-            } 
+                }).catch((err) => {
+                    console.log("Error", err.response.data);
+                    setCountStatus(err.response.data);
+                })  
+            }
         if(selected == 'Pro') {
             result = axios(`${appConfig.shopify.endpoint}${appConfig.shopify.envpath}${appConfig.shopify.billing}?billingstate=pro&price=59.99&shop=${originCookie}`)    
             .then(res => {
                 console.log(res.data);
-                window.open(res.data, "_parent");
+                window.open(res.data, "_parent"); 
+            }).catch((err) => {
+                console.log("Error", err.response.data);
+                setCountStatus(err.response.data);
             })    
         }
         if(selected == 'Premium') {
@@ -62,12 +83,15 @@ const Subscriptions = () => {
             .then(res => {
                 console.log(res.data);
                 window.open(res.data, "_parent");
-            })       
+            }).catch((err) => {
+                console.log("Error", err.response.data);
+                setCountStatus(err.response.data);
+            })        
         }
         console.log(selected);
     }
 
-    console.log(selected);
+    //console.log(countStatus.text);
 
     useEffect(async() => {
         const res = await axios(`${appConfig.shopify.endpoint}${appConfig.shopify.envpath}${appConfig.shopify.billing}?status=info&shop=${originCookie}`);
@@ -80,38 +104,42 @@ const Subscriptions = () => {
 
       //console.log(selected);
 
-
       useEffect(async () => {
-          if(selectedState.billing_current_status){
-            setSelected([selectedState.billing_current_status])
+          if(selectedState.billing_current_plan){
+            setSelected([selectedState.billing_current_plan])
+          }
+          if(selectedState.billing_current_status === "Expired"){
+            setSelected([selectedState.billing_current_plan])
           }
       }, [selectedState]);
+      
 
     return (
         <Page title="Subscriptions">
             <Layout>
                 <Layout.AnnotatedSection title="Billing" description="Select a different plan, click on Change Plan and approve the new subscription to change your plan.">
                     <Layout.Section>
-                    <Card title="Your Plan" sectioned>
-                    <TextStyle>Current Plan</TextStyle><br/>
-                    <Badge status="success">{selectedState.display_status}</Badge><br/>
-                    {selectedState.display_status == 'Expired' ? <TextStyle variation="strong">your current plan '{selectedState.billing_previous_status}' is Expired. Please Change to next plan</TextStyle> : null}<br/><br/>
+                    <Card title="Plans" sectioned>
+                    <Heading element="h3">Current Plan</Heading>
+                    {selectedState.billing_current_status === 'Expired' ? <Badge status="warning"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge> : <Badge status="success"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge>}<br/>
+                    {selectedState.billing_current_status === 'Expired' ? <TextStyle variation="strong">Your current plan '{selectedState.billing_current_plan}' is Expired. Please choose a different plan:</TextStyle> : null}<br/><br/>
                     <ChoiceList
-                    title="Select any plan"
+                    title="Select a plan"
                     choices={[
-                        {...TrialPlan,disabled:selectedState.billing_current_status && (selectedState.billing_current_status.includes('Basic') || selectedState.billing_current_status.includes('Pro') || selectedState.billing_current_status.includes('Premium')) },
-                        {...BasicPlan,disabled: selectedState.billing_current_status && (selectedState.billing_current_status.includes('Pro') || selectedState.billing_current_status.includes('Premium')) },
-                        {...ProPlan,disabled: selectedState.billing_current_status && (selectedState.billing_current_status.includes('Premium')) },
-                        PremiumPlan,
+                        {...TrialPlan,disabled: selectedState.billing_current_status === 'Expired' || selectedState.billing_current_plan && (selectedState.billing_current_plan.includes('Basic') || selectedState.billing_current_plan.includes('Pro') || selectedState.billing_current_plan.includes('Premium')) },
+                        {...BasicPlan, },
+                        {...ProPlan},
+                        {...PremiumPlan},
                     ]}
                     selected={selected}
                     //error="Change your plan"
                     onChange={handleChange}
                     /><br/>
-                    <Button primary onClick={() => 
-                        {handleClick();
-                        }
-                        }>Change Plan</Button>
+                   { showBanner ? <Banner
+                        title={countStatus.text}
+                        status="warning"
+                        /> : null}<br/>
+                    <Button primary disabled={selected[0] === selectedState.billing_current_plan || showBanner} onClick={() => {handleClick();}}>Change Plan</Button>
                     </Card>
                     </Layout.Section>
                 </Layout.AnnotatedSection>
