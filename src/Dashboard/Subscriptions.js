89c03ce02bf18f3fbd0bc8176dfca8b1940fa046
NagaSatish <dev02@retrieveai.com>
2021-07-22 14:38:19 +0530

    implemented condition for Trial Plan

--- src/Dashboard/Subscriptions.js
+++ src/Dashboard/Subscriptions.js
@@ -25,6 +25,7 @@ const Subscriptions = () => {
     const [selected, setSelected] = useState([]);
 
     const planLimits = {
+        Trial: 1000,
         Basic: 5000,
         Pro: 50000,
         Premium: 200000
@@ -37,16 +38,19 @@ const Subscriptions = () => {
 
     useEffect(() => {
         console.log(planLimits[selected[0]], countStatus.current_month_visitors_count );
-        if(countStatus.current_month_visitors_count > planLimits[selected[0]]){
+        if(selected[0] === countStatus.selected_plan && countStatus.billing_current_plan_status === "Expired" || countStatus.current_month_visitors_count > planLimits[selected[0]]){
             setShowBanner(true)
-        }else setShowBanner(false)
+        }
+        else setShowBanner(false)
       }, [selected, countStatus]);
+
+      //console.log(selected[0]);
       
 
     //console.log(selected);
 
     const TrialPlan = {label: 'Free Trial', value: 'Trial', helpText:
-    'Free Trial for 14 days',};
+    'Free Trial for 14 days | Up to 1000 Site Visitors | Sales, Support and Order Tracking Chats | AI Chat Interactions'};
     const BasicPlan = {label: 'Basic', value: 'Basic',  helpText:
     '$24.99 per month | Up to 5,000 Monthly Site Visitors | Unlimited Sales,Support and Order Tracking Chats | Unlimited AI Chat Interactions',};
     const ProPlan = {label: 'Pro', value: 'Pro',  helpText:
@@ -57,6 +61,17 @@ const Subscriptions = () => {
     const handleClick = () => {
         let result;
         console.log(selected);
+        if(selected == 'Trial') {
+            result = axios.get(
+               `${appConfig.shopify.endpoint}${appConfig.shopify.envpath}${appConfig.shopify.billing}?billingstate=trial&price=0&shop=${originCookie}`
+            ).then(res => {
+                console.log(res.data);
+                window.open(res.data, "_parent");
+                }).catch((err) => {
+                    console.log("Error", err.response.data);
+                    setCountStatus(err.response.data);
+                })  
+            }
         if(selected == 'Basic') {
             result = axios.get(
                `${appConfig.shopify.endpoint}${appConfig.shopify.envpath}${appConfig.shopify.billing}?billingstate=basic&price=24.99&shop=${originCookie}`
@@ -108,7 +123,7 @@ const Subscriptions = () => {
           if(selectedState.billing_current_plan){
             setSelected([selectedState.billing_current_plan])
           }
-          if(selectedState.billing_current_status === "Expired"){
+          if(selectedState.billing_current_plan_status === "Expired"){
             setSelected([selectedState.billing_current_plan])
           }
       }, [selectedState]);
@@ -121,12 +136,12 @@ const Subscriptions = () => {
                     <Layout.Section>
                     <Card title="Plans" sectioned>
                     <Heading element="h3">Current Plan</Heading>
-                    {selectedState.billing_current_status === 'Expired' ? <Badge status="warning"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge> : <Badge status="success"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge>}<br/>
-                    {selectedState.billing_current_status === 'Expired' ? <TextStyle variation="strong">Your current plan '{selectedState.billing_current_plan}' is Expired. Please choose a different plan:</TextStyle> : null}<br/><br/>
+                    {selectedState.billing_current_plan_status === 'Expired' ? <Badge status="warning"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge> : <Badge status="success"><Heading element="h4">{selectedState.billing_plan_description}</Heading></Badge>}<br/>
+                    {selectedState.billing_current_plan_status === 'Expired' ? <TextStyle variation="strong">Your current plan '{selectedState.billing_current_plan}' is Expired. Please choose a different plan:</TextStyle> : null}<br/><br/>
                     <ChoiceList
                     title="Select a plan"
                     choices={[
-                        {...TrialPlan,disabled: selectedState.billing_current_status === 'Expired' || selectedState.billing_current_plan && (selectedState.billing_current_plan.includes('Basic') || selectedState.billing_current_plan.includes('Pro') || selectedState.billing_current_plan.includes('Premium')) },
+                        {...TrialPlan,},
                         {...BasicPlan, },
                         {...ProPlan},
                         {...PremiumPlan},
